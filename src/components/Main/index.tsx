61c344700f358e983676cf784ba47587434ed2b2
dev-fernandes <rodrigorcfernandes@gmail.com>
2021-05-16 10:40:15 -0300

    Boilerplate first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'Advanced React',
+  description = 'TypeScript, ReactJS, NextJS and Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Image of an atom" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer in front of a screen with code"
+    />
+  </S.Wrapper>
 )
 
 export default Main
